96bacb586dbbc2168c047d45e52f63c0177b8f6a
Nikolai Kristiansen <nikolaik@gmail.com>
2012-01-05 22:28:39 +0100

    Register buttons.

--- main/views.py
+++ main/views.py
@@ -6,4 +6,6 @@ from django.template import RequestContext
 from models import *
 
 def home(request):
-    return render_to_response('home.html', locals())
+    products = Product.objects.all()
+    users = User.objects.all()
+    return render_to_response('home.html', locals(), context_instance=RequestContext(request))
