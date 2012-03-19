07f37e57df62be883c5292b46ff104c2ac19ca8b
Nikolai Kristiansen <nikolaik@gmail.com>
2012-03-19 23:50:53 +0100

    Log: Adds show all

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('main.views',
     url(r'^register/$', 'register', name='register'),
     url(r'^deposit/$', 'deposit', name='deposit'),
     url(r'^log/$', 'log', name='log'),
+    url(r'^log/all$', 'log', kwargs={'limit': None}, name='log-all'),
 )
