368228e5fa812a40d1f353fa87819c0221f0bcb0
Nikolai Kristiansen <nikolaik@gmail.com>
2012-11-10 23:26:59 +0100

    Newer bootstrap w/ fixes

---
+++ static/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+# Set this to the root of your project when deployed:
+http_path = "/static/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :compact
+relative_assets = true
+line_comments = false
