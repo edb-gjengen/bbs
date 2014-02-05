d02d22f52d76fdb6c551b65bfd0688dbd0f0e9a0
Nikolai Kristiansen <nikolaik@gmail.com>
2014-02-05 19:37:20 +0100

    Small fixes

--- main/utils.py
+++ main/utils.py
@@ -2,4 +2,4 @@ def format_username(firstname, lastname):
     first = firstname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
     last = lastname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
 
-    return "{}{}".format(first, last)
+    return "{0}{0}".format(first, last)
