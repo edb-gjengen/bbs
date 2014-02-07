921dc92b07be12874011701df5897f408f05337e
Nikolai Kristiansen <nikolaik@gmail.com>
2014-02-07 10:43:53 +0100

    /sigh

--- main/utils.py
+++ main/utils.py
@@ -5,7 +5,7 @@ def format_username(firstname, lastname):
     first = firstname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
     last = lastname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
 
-    return "{0}{0}".format(first, last)
+    return "{0}{1}".format(first, last)
 
 def users_with_perm(perm_name):
     return User.objects.filter(
