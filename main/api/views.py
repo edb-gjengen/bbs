21c4f11ebcd7f5158d7e260f9fa8094261b3d30c
Nikolai Kristiansen <nikolaik@gmail.com>
2015-11-30 11:31:00 +0100

    go back

--- main/api/views.py
+++ main/api/views.py
@@ -13,9 +13,8 @@ class ProductViewSet(mixins.ListModelMixin,
                      GenericViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    permission_classes = [AllowAny]
 
-    @list_route(methods=['get'], permission_classes=[AllowAny])
+    @list_route(methods=['get'])
     def group_by_user(self, request):
         products = self.get_queryset().filter(active=True)
         serializer = ProductStatSerializer(products, many=True)
