8a4a9fcb26a4abc861da3fb013b9a7123ecfb231
Nikolai Kristiansen <nikolaik@gmail.com>
2016-04-13 21:16:22 +0200

    User creation fixes

--- main/signals.py
+++ main/signals.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from main.models import UserProfile
 
 
-@receiver(post_save, sender=get_user_model(), dispatch_uid='randomz')
+@receiver(post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
     # Create a new UserProfile object when we create a new User.
     if created:
--- main/utils.py
+++ main/utils.py
@@ -4,8 +4,8 @@ from django.db.models import Q
 
 
 def format_username(firstname, lastname):
-    first = firstname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
-    last = lastname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore')
+    first = firstname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore').decode('utf-8')
+    last = lastname.lower().strip().replace(" ", "")[:6].encode('ascii', 'ignore').decode('utf-8')
 
     return "{0}{1}".format(first, last)
 
