f0e633163035123deca4677eeb9efbfd28d406c8
Toralf Neby Berge <toralf.berge@gmail.com>
2017-05-03 22:15:00 +0200

    fix

--- main/models.py
+++ main/models.py
@@ -113,7 +113,7 @@ class InventoryTransaction(models.Model):
         return self.amount * self.unit_price
 
     def __str__(self):
-        return "{}: {}: {}. {} stykker ({} kr per)".format(
+        return "{}: {}: {}. {:.0f} stykker ({} kr per)".format(
             self.id,
             self.user,
             self.product,
