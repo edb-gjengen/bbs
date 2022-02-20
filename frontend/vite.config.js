faca98b7f97d13e24d376afbcd3c643f958b8b64
Nikolai R Kristiansen <nikolaik@gmail.com>
2022-02-20 01:20:41 +0100

    🔧 Reference static assests from /

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV !== "production" ? "/" : "/static/modern/",
   plugins: [svgr()],
   define: {
     __DEV__: process.env.NODE_ENV !== "production",
